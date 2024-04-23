3c4fad650ab9644d908586a106d2969e319b5c27
dh0615 <0615dh@gmail.com>
2024-04-23 18:25:36 +0900

    240423

---
+++ mealkit/javascript/search.js
@@ -0,0 +1,16 @@
+const allClear = document.querySelector(".btn-all-clear");
+
+const searchItems = document.querySelector(".search-recent .items");
+
+allClear.addEventListener("click", () => {
+  searchItems.style.display = "none";
+});
+
+const eachItems = document.querySelectorAll(".search-recent .btn-clear");
+console.log(eachItems);
+
+eachItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    this.parentNode.style.display = "none";
+  });
+});
