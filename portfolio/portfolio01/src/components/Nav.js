e36f3b67647665fe78f06256c3077cab71b73b46
dh0615 <0615dh@gmail.com>
2024-05-20 07:29:35 +0900

    240520

--- portfolio/portfolio01/src/components/Nav.js
+++ portfolio/portfolio01/src/components/Nav.js
@@ -1,54 +1,127 @@
-import React from "react";
+
+import React, { useState } from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import { faBars, faX, faXmark } from "@fortawesome/free-solid-svg-icons";
 
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background: grey;
-`;
+// const Wrapper = styled.div`
+//   width: 100%;
+//   height: 100vh;
+//   background: grey;
+// `;
 
-const Header = styled.div`
-  width: 90vw;
-  height: 60px;
-  background: pink;
-  margin: 0 auto;
-`;
+// const Header = styled.div`
+//   width: 100%;
+//   height: 60px;
+//   background: pink;
+//   margin: 0 auto;
+  
+// `;
 
-const LogoImg = styled.div`
-  width: 100px;
-  height: 60px;
-  background: green;
-`;
+// const LogoImg = styled.div`
+//   width: 100px;
+//   height: 60px;
+//   background: green;
+// `;
 
-const MenuBar = styled.div`
+// const MenuBar = styled.div`
+// width : 90%;
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   margin: 0 auto;
+//   .toggle{
+//     display:block;
+//   }
+//   .headerMenuList {
+//     display: ${(props) => (props.isToggled ? "flex" : "none")};
+//     justify-content: space-around;
+//     align-items: center;
+//     border: 1px solid #f00;
+//     gap: 20px;
+//     flex-direction: column;
+//     width: 100%;
+//     background-color: black;
+//   }
+// `;
+
+const Wrapper = styled.div``;
+const Header = styled.div`max-width: 1280px;
+margin: 0 auto;
+display: flex;
+justify-content: space-between;
+align-items: center;
+color: white;
+background-color: black;
+
+.logo {
+  margin: 0 1rem;
+  font-size: 2rem;
+}
+
+.headerMenuList{
+  list-style: none;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+}
 
-  ul {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    border: 1px solid #f00;
-    gap: 20px;
+li {
+  padding: 0 1rem;
+}
+
+.toggle {
+  display: none;
+  font-size: 1.5rem;
+  padding: 1rem 1rem;
+}
+
+
+@media screen and (max-width: 768px) {
+  flex-wrap: wrap;
+
+  .headerMenuList {
+    display: ${(props) => (props.toggleMenu ? "flex" : "none")};
+    flex-direction: column;
+    width: 100%;
+    background-color: black;
   }
+
+  .headerMenuList li {
+    margin: 1rem 0;
+    padding: 0;
+  }
+
+  .toggle {
+    display: block;
+  }
+
+}
 `;
+const MenuBar = styled.div``;
+const LogoImg = styled.div``;
+
+
 
 const Nav = () => {
+  const [toggleMenu, setToggleMenu] = useState();
+
   return (
     <div>
       <Wrapper>
-        <Header>
+        <Header toggleMenu = {toggleMenu}>
           <MenuBar>
-            <LogoImg>
-              <ul>
-                <li style={{ listStyle: "none" }}>
-                                               {" "}
-                </li>
-              </ul>
-            </LogoImg>
+            <LogoImg></LogoImg>
+            <div className="toggle" onClick={() => {setToggleMenu(!toggleMenu)}}>
+              <FontAwesomeIcon icon = {!toggleMenu ? faBars : faX} />
+            </div>
+            <ul className="headerMenuList">
+              <li>About</li>
+              <li>Skill</li>
+              <li>Portfolio</li>
+              <li>Clone</li>
+              <li>Contact</li>
+            </ul>
           </MenuBar>
         </Header>
       </Wrapper>
--- portfolio/portfolio01/src/pages/Cover.js
+++ portfolio/portfolio01/src/pages/Cover.js
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import '../App'
+
+
+const Wrapper = styled.div`
+
+
+`
 
 const Cover = () => {
-  return <div>Cover</div>;
+  return <div>
+    <Wrapper>
+      <div className="cover">
+        <h1></h1>
+      </div>
+    </Wrapper>
+  </div>;
 };
 
 export default Cover;
